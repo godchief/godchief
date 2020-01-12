7d6de1997842222374e89f27a5329e5e5bb89ba5
godchief <godchief@naver.com>
2020-01-12 21:56:01 +0900

    no.1

---
+++ syntax/variable.js
@@ -0,0 +1,5 @@
+var a = 1;
+console.log(a);
+var a = '2123123123123';
+console.log(a.length);
+//1 = 2;
---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'realstun'
+var letter = name+ ' we are the '+name+' future'
+console.log(letter);
